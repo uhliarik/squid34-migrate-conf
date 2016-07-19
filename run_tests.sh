3b08d39c4bdbbb38975dd79cc1b07ac50608bb98
Luboš Uhliarik <luhliari@redhat.com>
2016-07-19 14:53:26 +0200

    Fixed bug - changes would not be written to the result test dir

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 #
 # This is just simple test file, feel free to modify it
 #
-#
+# Lubos Uhliarik
 #
 TESTS_DIR="tests"
 BASE_DIR=$(pwd)
@@ -14,11 +14,11 @@ rm -rf "$RESULT_DIR"
 # create result directory
 cp -R "$TESTS_DIR" $RESULT_DIR
 
-# execture all tests
+# execute all tests
 for i in "$RESULT_DIR"/* ; do
     echo ">>>>> Running test $(basename $i) ..."
     cd "$i"
-    python "$BASE_DIR/squid-migrate-conf.py" --conf "squid.conf"
+    python "$BASE_DIR/squid-migrate-conf.py" --conf "squid.conf" --write-changes
     cd -
     echo ">>>>> Finished"
 done
